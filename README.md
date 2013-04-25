08b8867de4885fa8b6952523f7910913886a533e
Alex Muller <alex@mullr.net>
2013-04-25 15:45:34 +0100

    Functional booking-viewer front-end

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # French house
 
 A Refinery CMS-backed app.
+
+## Ideas
+
+- Include a weather forecast on the booking calendar?
--- vendor/extensions/bookings/app/controllers/refinery/bookings/bookings_controller.rb
+++ vendor/extensions/bookings/app/controllers/refinery/bookings/bookings_controller.rb
@@ -2,10 +2,22 @@ module Refinery
   module Bookings
     class BookingsController < ::ApplicationController
 
-      before_filter :find_all_bookings
       before_filter :find_page
 
       def index
+        date_from = Date.today.beginning_of_week
+        date_to = (date_from + 11.weeks).end_of_week
+        bookings = Booking.where("start_date <= ? AND end_date >= ?", date_to, date_from)
+        booked_dates = bookings.collect_concat do |b|
+          (b.start_date..b.end_date).map { |d| Date.new(d.year, d.month, d.day) }
+        end
+        @days = (date_from..date_to).map do |d|
+          date = Date.new(d.year, d.month, d.day)
+          {
+            :date => date,
+            :booked => booked_dates.include?(date) ? true : false
+          }
+        end
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @booking in the line below:
         present(@page)
@@ -20,10 +32,6 @@ module Refinery
       end
 
     protected
-
-      def find_all_bookings
-        @bookings = Booking.order('position ASC')
-      end
 
       def find_page
         @page = ::Refinery::Page.where(:link_url => "/bookings").first
